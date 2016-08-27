2176ce883609a2171fa5fa1436d9675e26111360
HP <HP@Din>
2016-08-27 13:32:30 +0800

    search on for json

--- Tourcan/src/com/tourcan/att/controller/TestAttServlet.java
+++ Tourcan/src/com/tourcan/att/controller/TestAttServlet.java
@@ -37,7 +37,7 @@ public class TestAttServlet extends HttpServlet {
 //				if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
 		String str = request.getParameter("attno");
 		String att_name = request.getParameter("attname");
-		PrintWriter out = response.getWriter();
+//		PrintWriter out = response.getWriter();
 		if(str!=null){
 			List<String> errorMsgs1 = new LinkedList<String>();
 			// Store this set in the request scope, in case we need to
@@ -76,71 +76,81 @@ public class TestAttServlet extends HttpServlet {
 						JSONArray list = new JSONArray();
 						request.setAttribute("attVO", attVO); // 資料庫取出的attVO物件,存入req
 						jsob.addProperty("id", attVO.getAtt_id());
+						jsob.addProperty("name", attVO.getAtt_name());
 						jsob.addProperty("addr", attVO.getAtt_addr());
+						jsob.addProperty("intro", attVO.getAtt_intro());
+						jsob.addProperty("open", attVO.getAtt_open());
+						jsob.addProperty("phone", attVO.getAtt_phone());
+						jsob.addProperty("url", attVO.getAtt_url());
+						jsob.addProperty("price", attVO.getAtt_price());
+						jsob.addProperty("staytime", attVO.getAtt_staytime());
+						jsob.addProperty("region_id", attVO.getRegionVO().getRegion_id());
+						
 						System.out.println(jsob);
+						
 						response.getWriter().println(jsob.toString());;
 						
-						out.println(jsob);
+//						out.println(jsob);
 //						String url = "/result/listOneAtt.jsp";
 //						RequestDispatcher successView = request.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
 //						successView.forward(request, response);
 
-				
+		}
+		}
 					
 					
 					
 					
-					
-		}else if(att_name!=null){
-			List<String> errorMsgs1 = new LinkedList<String>();
-			request.setAttribute("errorMsgs", errorMsgs1);
-			try {
-						//***************************1.接收請求參數 - 輸入格式的錯誤處理**********************//*
-				if (att_name == null || (att_name.trim()).length() == 0) {
-				errorMsgs1.add("請輸入attname");
-			}
-			System.out.println("s1="+att_name);
-							// Send the use back to the form, if there were errors
-						if (!errorMsgs1.isEmpty()) {
-							RequestDispatcher failureView = request
-							.getRequestDispatcher("/att/query_One_Att.jsp");
-							failureView.forward(request, response);
-								return;//程式中斷
-				}			
-									//***************************2.開始查詢資料*****************************************//*
-							
-				AttService asv =new AttService();				
-				List<AttVO> avo = asv.getAllByName(att_name);
-									
-				if (avo == null) {
-				errorMsgs1.add("查無資料");
-					}
-									// Send the use back to the form, if there were errors
-				if (!errorMsgs1.isEmpty()) {
-					RequestDispatcher failureView = request
-					.getRequestDispatcher("/query_One_Att.jsp");
-					failureView.forward(request, response);
-					return;//程式中斷
-									}
-									
-							//***************************3.查詢完成,準備轉交(Send the Success view)*************//*
-					request.setAttribute("list", avo); // 資料庫取出的empVO物件,存入req
-					String url = "/att/query_All_att_for_name.jsp";
-					RequestDispatcher successView = request.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-					successView.forward(request, response);
-			
-									//***************************其他可能的錯誤處理*************************************//*
-				} catch (Exception e) {
-					errorMsgs1.add("無法取得資料:" + e.getMessage());
-									RequestDispatcher failureView = request
-											.getRequestDispatcher("/att/query_One_Att.jsp");
-									failureView.forward(request, response);
-								}
-							}
-											
-					
+//		}else if(att_name!=null){
+//			List<String> errorMsgs1 = new LinkedList<String>();
+//			request.setAttribute("errorMsgs", errorMsgs1);
+//			try {
+//						//***************************1.接收請求參數 - 輸入格式的錯誤處理**********************//*
+//				if (att_name == null || (att_name.trim()).length() == 0) {
+//				errorMsgs1.add("請輸入attname");
+//			}
+//			System.out.println("s1="+att_name);
+//							// Send the use back to the form, if there were errors
+//						if (!errorMsgs1.isEmpty()) {
+//							RequestDispatcher failureView = request
+//							.getRequestDispatcher("/att/query_One_Att.jsp");
+//							failureView.forward(request, response);
+//								return;//程式中斷
+//				}			
+//									//***************************2.開始查詢資料*****************************************//*
+//							
+//				AttService asv =new AttService();				
+//				List<AttVO> avo = asv.getAllByName(att_name);
+//									
+//				if (avo == null) {
+//				errorMsgs1.add("查無資料");
+//					}
+//									// Send the use back to the form, if there were errors
+//				if (!errorMsgs1.isEmpty()) {
+//					RequestDispatcher failureView = request
+//					.getRequestDispatcher("/query_One_Att.jsp");
+//					failureView.forward(request, response);
+//					return;//程式中斷
+//									}
+//									
+//							//***************************3.查詢完成,準備轉交(Send the Success view)*************//*
+//					request.setAttribute("list", avo); // 資料庫取出的empVO物件,存入req
+//					String url = "/att/query_All_att_for_name.jsp";
+//					RequestDispatcher successView = request.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
+//					successView.forward(request, response);
+//			
+//									//***************************其他可能的錯誤處理*************************************//*
+//				} catch (Exception e) {
+//					errorMsgs1.add("無法取得資料:" + e.getMessage());
+//									RequestDispatcher failureView = request
+//											.getRequestDispatcher("/att/query_One_Att.jsp");
+//									failureView.forward(request, response);
+//								}
+//							}
+//											
+//					
 		
-				}
+//				}
 	
 		
 
