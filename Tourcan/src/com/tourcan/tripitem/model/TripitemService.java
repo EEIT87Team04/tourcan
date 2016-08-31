168882621e36527a1c18e36f10b9add0dd525e16
george <george@192.168.133.1>
2016-09-01 00:26:39 +0800

    trip,tripitem model OK

--- Tourcan/src/com/tourcan/tripitem/model/TripitemService.java
+++ Tourcan/src/com/tourcan/tripitem/model/TripitemService.java
@@ -1,10 +1,34 @@
 package com.tourcan.tripitem.model;
 
+import java.util.List;
+
 public class TripitemService {
 
 	private TripitemDAO_interface dao;
 
 	public TripitemService() {
 		dao = new TripitemDAO();
+	}
+	
+	public TripitemVO insert(TripitemVO tripitemVO) {
+			dao.insert(tripitemVO);
+			return tripitemVO;
+	}
+	
+	public TripitemVO update(TripitemVO tripitemVO) {
+		dao.update(tripitemVO);
+		return tripitemVO;
+	}
+	
+	public void delete(Integer tripitem_serial) {
+		dao.delete(tripitem_serial);
+	}
+	
+	public TripitemVO findById(Integer tripitem_serial){
+		return dao.findById(tripitem_serial);
+	}
+	
+	public List<TripitemVO> getAll(){
+		return dao.getAll();
 	}
 }
