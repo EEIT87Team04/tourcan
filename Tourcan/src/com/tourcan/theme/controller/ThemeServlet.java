7de2546dbb7394f53d2f36fc42ca945c038b8153
Student <Student@EEIT-PC>
2016-09-04 09:32:42 +0800

    test

--- Tourcan/src/com/tourcan/theme/controller/ThemeServlet.java
+++ Tourcan/src/com/tourcan/theme/controller/ThemeServlet.java
@@ -120,8 +120,8 @@ public class ThemeServlet  extends HttpServlet{
 	}
   		
   	
-  	
-  	protected void doGPost(HttpServletRequest req, HttpServletResponse resp)
+  	@Override
+  	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
   		req.setCharacterEncoding("UTF-8");
   		resp.setCharacterEncoding("UTF-8");
