f37a9c31e8d97c0e6c9fb0c042336ad126f0e6ef
劉慶和 <pingu8007@gmail.com>
2016-09-20 10:54:19 +0800

    Change to spring-based hibernate.
    HibernateUtil will using SessionFactory comes from spring from now on.
    No more duplicated hibernate.cfg.xml, weee!!

--- Tourcan/src/hibernate/util/HibernateUtil.java
+++ Tourcan/src/hibernate/util/HibernateUtil.java
@@ -6,25 +6,28 @@
 package hibernate.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
 
-    static {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+	// static {
+	// try {
+	// // Create the SessionFactory from hibernate.cfg.xml
+	// sessionFactory = new Configuration().configure().buildSessionFactory();
+	// } catch (Throwable ex) {
+	// // Make sure you log the exception, as it might be swallowed
+	// System.err.println("Initial SessionFactory creation failed." + ex);
+	// throw new ExceptionInInitializerError(ex);
+	// }
+	// }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+	public void setSessionFactory(SessionFactory factory) {
+		HibernateUtil.sessionFactory = factory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 
 }
